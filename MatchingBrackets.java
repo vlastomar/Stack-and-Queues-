7303aed41c6cb8cdd228a89143352aba9468efae
VSM <vlad24@abv.bg>
2020-08-24 08:09:09 +0300

    Create MatchingBrackets.java

---
+++ MatchingBrackets.java
@@ -0,0 +1,27 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (ch == '(')
+                stack.push(i);
+            else if (ch == ')') {
+                int startIndex = stack.pop();
+                String contents = input.substring(startIndex, i + 1);
+                System.out.println(contents);
+            }
+
+        }
+
+    }
+}
